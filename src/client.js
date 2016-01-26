ed40537ac9aef382bda44e750648dc7a6eb8c321
Philippe Voinov <philippevoinov@gmail.com>
2016-01-26 21:02:03 +0100

    Running actions.

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
 "use strict";
-var io = require('socket.io-client');
+var io = require('socket.io-client'),
+  _ = require('lodash');
 
 class Client {
   constructor (config) {
@@ -7,20 +8,37 @@ class Client {
   }
 
   connect (url, options) {
+    return this._connect(url, options).then(() => {
+      this._configureSocket();
+      return this._register();
+    });
+  }
+
+  _connect (url, options) {
     this._socket = io(url, options);
     let deferred = Promise.defer();
     this._socket.on('connect', () => {deferred.resolve();});
     this._socket.on('connect_error', () => {deferred.reject();});
-    this._configureSocket();
-    this._register();
     return deferred.promise;
   }
 
   _configureSocket () {
+    this._socket.on('runAction', this._runAction.bind(this));
   }
 
   _register () {
-    this._socket.emit('register', this.config);
+    this._socket.emit('register', {
+      profiles: _.keys(this.config.profiles)
+    });
+  }
+
+  _runAction (options) {
+    try {
+      this.config.profiles[options.profile].actions[options.action](options.data);
+    } catch (e) {
+      console.error('Failed to run action', options);
+      console.error(e.stack);
+    }
   }
 }
 
--- test/unit/client.js
+++ test/unit/client.js
@@ -20,30 +20,94 @@ describe('client', function () {
     };
     Client = proxyquire('../../src/client.js', deps);
   });
+  var config;
+  beforeEach(function () {
+    config = {
+      profiles: {
+        'testProfile1': {
+          actions: {
+            'testAction': sinon.spy()
+          }
+        },
+        'testProfile2': {}
+      }
+    };
+  });
   it('exposes client configuration', function () {
-    let config = {};
     let client = new Client(config);
     expect(client.config).to.equal(config);
   });
   it('passes through socket options', function () {
-    let client = new Client({}),
+    let client = new Client(config),
       socketOptions = {},
       connected = client.connect('url', socketOptions);
     expect(socketMock.constructorStub).to.have.been.calledWith('url', socketOptions);
   });
   it('rejects promise on connection error', function () {
-    let client = new Client({}),
+    let client = new Client(config),
       connected = client.connect();
     socketMock.socket._handlers.connect_error();
     return expect(connected).to.be.rejected;
   });
-  it('registers with central on connection, passing the whole config', function () {
-    let config = {};
-    let client = new Client(config),
+  describe('(successfully connected)', function () {
+    var client, connected;
+    beforeEach(function () {
+      client = new Client(config);
       connected = client.connect();
-    socketMock.socket._handlers.connect();
-    return expect(connected).to.be.fulfilled.then(() => {
-      expect(socketMock.socket.emit).to.have.been.calledWith('register', config);
+      socketMock.socket._handlers.connect();
+      return expect(connected).to.be.fulfilled;
+    });
+    it('registers with central on connection, passing supported profiles', function () {
+      expect(socketMock.socket.emit).to.have.been.calledWithMatch('register', {
+        profiles: ['testProfile1', 'testProfile2']
+      });
+    });
+    it('runs action when requested', function () {
+      let data = {};
+      socketMock.socket._handlers.runAction({
+        profile: 'testProfile1',
+        action: 'testAction',
+        data: data
+      });
+      expect(config.profiles.testProfile1.actions.testAction).to.have.been.calledWith(data);
+    });
+    describe('(crash tests)', function () {
+      var sandbox;
+      beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(console, "error");
+      });
+      afterEach(function () {
+        sandbox.restore();
+      });
+      it('does not crash if profile is not found', function () {
+        socketMock.socket._handlers.runAction({
+          profile: 'saoenuhtskbbth',
+          action: 'testAction',
+          data: {}
+        });
+      });
+      it('does not crash if action is not found', function () {
+        socketMock.socket._handlers.runAction({
+          profile: 'testProfile1',
+          action: 'lckga,th,r.b',
+          data: {}
+        });
+      });
+      it('does not crash action crashes', function () {
+        config.profiles.testProfile1.actions.testAction = sinon.stub().throws();
+        socketMock.socket._handlers.runAction({
+          profile: 'testProfile1',
+          action: 'testAction'
+        });
+      });
+    });
+    it('allows undefined data paths to passed to action', function () {
+      socketMock.socket._handlers.runAction({
+        profile: 'testProfile1',
+        action: 'testAction'
+      });
+      expect(config.profiles.testProfile1.actions.testAction).to.have.been.calledOnce;
     });
   });
 });
